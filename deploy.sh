f0a5fee01b81b644dbd54d8e286fb2dcce506397
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-12-27 18:30:52 +0530

    read params from params file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+aws ec2 delete-key-pair --key-name gltest
+rm -f gltest.pem
+aws ec2 create-key-pair \
+	--key-name gltest \
+	--query 'KeyMaterial' | sed -e 's/"//g' -e 's/\\n/\n/g' > gltest.pem
+chmod 400 gltest.pem
+
+
 aws cloudformation deploy \
 	--stack-name task1stack \
-	--parameter-overrides EmailAddress=sk.sanjiv@gmail.com \
+	--parameter-overrides file://params.json \
 	--template-file task1.json
