d4cde20b67c82c40697217eaedc974e1f1c44d83
Sanjiv Singh <sk.sanjiv@gmail.com>
2022-12-28 00:24:49 +0530

    updated scripts

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+BUCKET_NAME=sanjivsingh-gl-app-bucket
+
 aws ec2 delete-key-pair --key-name gltest
 rm -f gltest.pem
 
 aws cloudformation delete-stack \
 	--stack-name task1stack
+
+rm -f app.zip
+aws s3 rm s3://$BUCKET_NAME/app.zip
+aws s3 rb s3://$BUCKET_NAME
+
+aws cloudformation delete-stack \
+	--stack-name task2stack
+
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-aws ec2 delete-key-pair --key-name gltest
-rm -f gltest.pem
-aws ec2 create-key-pair \
+BUCKET_NAME=sanjivsingh-gl-app-bucket
+
+task1() {
+    aws ec2 create-key-pair \
 	--key-name gltest \
 	--query 'KeyMaterial' | sed -e 's/"//g' -e 's/\\n/\n/g' > gltest.pem
-chmod 400 gltest.pem
+    chmod 400 gltest.pem
 
 
-aws cloudformation deploy \
+    aws cloudformation deploy \
 	--stack-name task1stack \
 	--parameter-overrides file://params.json \
 	--template-file task1.json
+}
+
+task2() {
+    aws cloudformation deploy \
+	--stack-name task2stack \
+	--template-file task2.json
+
+    zip app.zip raw_data.py appspec.yml
+    aws s3 cp app.zip s3://$BUCKET_NAME/app.zip
+}
+
+#task1
+task2
