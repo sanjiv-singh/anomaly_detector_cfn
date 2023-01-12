940974d3eb86529c17a8befa52fbc42d99e4e80a
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-01-13 00:31:18 +0530

    moved codedeploy scripts to main deploy and cleanup scripts

--- cleanup.sh
+++ cleanup.sh
@@ -10,12 +10,33 @@ rm -f anomaly_lambda.zip
 aws s3 rm s3://$BUCKET_NAME/ --recursive
 aws s3 rb s3://$BUCKET_NAME
 
-aws cloudformation delete-stack \
-	--stack-name task3stack
+clean_stacks() {
+    aws cloudformation delete-stack \
+	    --stack-name task3stack
 
-aws cloudformation delete-stack \
-	--stack-name task2stack
+    aws cloudformation delete-stack \
+	    --stack-name task2stack
 
-aws cloudformation delete-stack \
-	--stack-name task1stack
+    aws cloudformation delete-stack \
+	    --stack-name task1stack
+}
 
+clean_codedeploy() {
+    aws deploy delete-deployment-group \
+        --application-name my-app \
+        --deployment-group-name my-app-group
+
+    aws deploy delete-application \
+        --application-name my-app
+
+    codedeploy_policy_arn="arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+
+    aws iam detach-role-policy \
+            --role-name codedeploy-service-role \
+            --policy-arn $codedeploy_policy_arn
+
+    aws iam delete-role --role-name codedeploy-service-role
+}
+
+clean_codedeploy
+clean_stacks
--- deploy.sh
+++ deploy.sh
@@ -18,13 +18,44 @@ task1() {
 
 }
 
+codedeploy_setup() {
+    aws iam create-role \
+        --role-name codedeploy-service-role \
+        --assume-role-policy-document file://code_policy.json
+
+    aws iam attach-role-policy \
+        --role-name codedeploy-service-role \
+        --policy-arn arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole
+    
+    role_arn=$(aws iam get-role --role-name codedeploy-service-role --query "Role.Arn" --output text)
+    
+    aws deploy create-application --application-name my-app
+    
+    aws deploy push \
+      --application-name my-app \
+      --s3-location s3://sanjivsingh-gl-app-bucket/app.zip \
+      --ignore-hidden-files
+    
+    aws deploy create-deployment-group \
+      --application-name my-app \
+      --deployment-group-name my-app-group \
+      --deployment-config-name CodeDeployDefault.OneAtATime \
+      --ec2-tag-filters Key=ec2-app-name,Value=my-app,Type=KEY_AND_VALUE \
+      --service-role-arn $role_arn
+    
+    aws deploy create-deployment \
+      --application-name my-app \
+      --deployment-config-name CodeDeployDefault.OneAtATime \
+      --deployment-group-name my-app-group \
+      --s3-location bucket=sanjivsingh-gl-app-bucket,bundleType=zip,key=app.zip
+}
+
 task2() {
     aws cloudformation deploy \
 	--stack-name task2stack \
 	--template-file task2.json
 
-    zip app.zip raw_data.py appspec.yml
-    aws s3 cp app.zip s3://$BUCKET_NAME/app.zip
+    codedeploy_setup
 }
 
 task3() {
@@ -45,4 +76,5 @@ task3() {
 task1
 task2
 task3
+
 
