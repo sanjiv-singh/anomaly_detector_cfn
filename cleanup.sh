53fcb210b90b611fe1fed1bc55b55b3f0f687622
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-01-13 00:18:37 +0530

    moify deploy and cleanup scripts to include task3

--- cleanup.sh
+++ cleanup.sh
@@ -4,14 +4,18 @@ BUCKET_NAME=sanjivsingh-gl-app-bucket
 
 aws ec2 delete-key-pair --key-name gltest
 rm -f gltest.pem
-
-aws cloudformation delete-stack \
-	--stack-name task1stack
-
 rm -f app.zip
-aws s3 rm s3://$BUCKET_NAME/app.zip
+rm -f anomaly_lambda.zip
+
+aws s3 rm s3://$BUCKET_NAME/ --recursive
 aws s3 rb s3://$BUCKET_NAME
 
 aws cloudformation delete-stack \
+	--stack-name task3stack
+
+aws cloudformation delete-stack \
 	--stack-name task2stack
+
+aws cloudformation delete-stack \
+	--stack-name task1stack
 
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUCKET_NAME=sanjivsingh-gl-app-bucket
+STREAM_NAME=m03p02_raw_data_stream
 
 task1() {
     aws ec2 create-key-pair \
@@ -26,5 +27,22 @@ task2() {
     aws s3 cp app.zip s3://$BUCKET_NAME/app.zip
 }
 
+task3() {
+
+    zip anomaly_lambda.zip anomaly_detection.py
+    aws s3 cp anomaly_lambda.zip s3://$BUCKET_NAME/anomaly_lambda.zip
+
+    stream_arn=$(aws kinesis describe-stream --stream-name "$STREAM_NAME" --query "StreamDescription.StreamARN" --output text)
+
+    aws cloudformation deploy \
+	--stack-name task3stack \
+        --parameter-overrides "KinesisStreamArn=$stream_arn" \
+	--template-file task3.json \
+	--capabilities CAPABILITY_IAM
+
+}
+
 task1
 task2
+task3
+
